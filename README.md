c34c3b4d4f7be52374f5010804ebf68be4c17efa
🔥 TommY ❄️ <lihroff@icloud.com>
2019-06-10 13:49:22 +0800

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+## verbose
+
+A fork of Facebook's invariant.
+
+A way to provide descriptive log in development for something that should be used carefully.
+
+## verbose(condition, message)
+
+```js
+const verbose = require('vebs');
+// import verbose form 'vebs'
+
+verbose(truely, 'You may have used it wrong.');
+// ⚠️ >: 1 You may have used it wrong.
+
+invariant(Falsey, 'This will not log');
+// nothing happened
+```
+
+**Note:** The verbose does not take effect in `production mode`, it is a dummy function.
+
+**Contrary:** Compared to invariant, the message will show only when condition is true.
---
+++ verbose.js
@@ -0,0 +1,59 @@
+/**
+ *
+ * Same as invariant but use console instead of Error.
+ *
+ * Use verbose() to report practical info which your program assumed.
+ *
+ * Provide sprintf-style format (only %s is supported) and arguments
+ * to provide information about what broke and what you were
+ * expecting.
+ *
+ * The verbose will not use in production mode.
+ */
+
+const isprod = process.env.NODE_ENV !== 'production';
+
+const verbose = function(condition, config, a, b, c) {
+  let format, level, prefix;
+
+  if (isprod) return;
+
+  if (config === undefined) {
+    throw new Error('verbose requires an error message argument');
+  }
+  if (typeof config === 'string') {
+    format = config;
+  } else {
+    format = config.format;
+    level = config.level;
+    prefix = config.prefix;
+  }
+
+  if (condition) {
+    // assert
+    // eslint-disable-next-line no-unused-vars
+    const [_1, _2, ...rest] = arguments;
+
+    let index = 0,
+      info;
+    const formatted = format.replace(/%s/g, function() {
+      return rest[index++];
+    });
+    info = prefix ? `${prefix} ${formatted}` : formatted;
+
+    // minify consoloe scope
+    switch (level) {
+      case 'error':
+      case 'warn':
+      case 'log': {
+        console[level](info);
+        break;
+      }
+      default: {
+        console.warn(info);
+      }
+    }
+  }
+};
+
+export default verbose;
